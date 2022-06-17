3b8bb360f578f17b59b2414672f3c2cb7ce4f92c
snypher <chvezk@amazon.com>
2022-06-17 23:08:34 +0000

    Add utils script to automate checking for details and errors from neptune bulk loads

---
+++ utils/neptune-bulk-loader-errors.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+source $HOME/.bash_profile
+
+cluster_ep="https://${NEPTUNE_CLUSTER_ENDPOINT}:${NEPTUNE_CLUSTER_PORT}/loader"
+
+for loadId in $(awscurl --service neptune-db --region ${SERVICE_REGION} "${cluster_ep}?details=true" | jq '.payload.loadIds[]');
+do
+        clean_loadId=$(echo -n ${loadId} | tr -d '"')
+        time=$(date -d@$(awscurl --service neptune-db --region ${SERVICE_REGION} "${cluster_ep}/${clean_loadId}?details=true" | jq '.payload.overallStatus.startTime'))
+        echo -n $time '-'
+        echo -n ${clean_loadId}: $(awscurl --service neptune-db --region ${SERVICE_REGION} "${cluster_ep}/${clean_loadId}?details=true" | jq '.payload.overallStatus.status')
+        echo -n ',S3 LOCATION': $(awscurl --service neptune-db --region ${SERVICE_REGION} "${cluster_ep}/${clean_loadId}?details=true" | jq '.payload.overallStatus.fullUri')
+        echo -n ',ERRORS': $(awscurl --service neptune-db --region ${SERVICE_REGION} "${cluster_ep}/${clean_loadId}?details=true&errors=true&page=1&errorsPerPage=3" | jq '.payload.errors.errorLogs')
+
+        echo
+done
---
+++ utils/neptune-last-bulk-load.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+source $HOME/.bash_profile
+
+cluster_ep="https://${NEPTUNE_CLUSTER_ENDPOINT}:${NEPTUNE_CLUSTER_PORT}/loader"
+
+last_loadId=$(awscurl --service neptune-db --region ${SERVICE_REGION} "${cluster_ep}?details=true" | jq '.payload.loadIds[0]')
+clean_loadId=$(echo -n ${last_loadId} | tr -d '"')
+awscurl --service neptune-db --region ${SERVICE_REGION} "${cluster_ep}/${clean_loadId}?details=true&errors=true&page=1&errorsPerPage=3"
+
+echo
