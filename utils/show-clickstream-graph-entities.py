586f7cc495f22ffa3d9135583f222c51873c4139
snypher <chvezk@amazon.com>
2022-06-27 13:40:03 +0000

    Update util script show-clickstream-graph-entities.py to accept an input parameter for CSV file

--- utils/show-clickstream-graph-entities.py
+++ utils/show-clickstream-graph-entities.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import argparse
 from random import randint
 
-csv_file_path = '/home/ec2-user/SageMaker/clickstream_data_inc.csv'
+# Defining input parameters
+parser = argparse.ArgumentParser()
+parser.add_argument('--csv-file', required=True, 
+    help='Full path to input CSV file with clickstream mock data')
+    
+# Parse input arguments
+args = parser.parse_args()
+
 col_names = [ 'session_id', 'client_ip', 'client_platform', 'canonical_url', 
     'domain_name', 'app_id', 'device_id', 'user_name', 'events', 
     'start_timestamp', 'start_event', 'end_timestamp', 'end_event', 
@@ -11,34 +19,43 @@ cols2 = [ 0, 1 ]
 cols3 = [ 0, 4 ]
 cols4 = [ 0, 6 ]
 cols5 = [ 0, 7 ]
-lines = len(pd.read_csv(csv_file_path))
-skip=randint(1,lines)
 
-print('Randomly selecting the clickstream record # {}'.format(skip))
-print('')
-
-df1 = pd.read_csv(csv_file_path, names=col_names, usecols=cols1, skiprows=skip, nrows=1)
-print('Sample data for SessionID vertex')
-print('--------------------------------')
-print(df1.squeeze())
-print('')
-df2 = pd.read_csv(csv_file_path, names=col_names, usecols=cols2, skiprows=skip, nrows=1)
-print('Sample data for SessionID to ClientIP edge')
-print('------------------------------------------')
-print(df2)
-print('')
-df3 = pd.read_csv(csv_file_path, names=col_names, usecols=cols3, skiprows=skip, nrows=1)
-print('Sample data for SessionID to DomainName edge')
-print('--------------------------------------------')
-print(df3)
-print('')
-df4 = pd.read_csv(csv_file_path, names=col_names, usecols=cols4, skiprows=skip, nrows=1)
-print('Sample data for SessionID to DeviceID edge')
-print('------------------------------------------')
-print(df4)
-print('')
-df5 = pd.read_csv(csv_file_path, names=col_names, usecols=cols5, skiprows=skip, nrows=1)
-print('Sample data for SessionID to User edge')
-print('--------------------------------------')
-print(df5)
-print('')
+try:
+    if args.csv_file is None:
+        raise Exception("A CSV file must be provided as input")
+    
+    csv_file_path = args.csv_file
+    lines = len(pd.read_csv(csv_file_path))
+    skip=randint(1,lines)
+    
+    print('Randomly selecting the clickstream record # {}'.format(skip))
+    print('')
+    
+    df1 = pd.read_csv(csv_file_path, names=col_names, usecols=cols1, skiprows=skip, nrows=1)
+    print('Sample data for SessionID vertex')
+    print('--------------------------------')
+    print(df1.squeeze())
+    print('')
+    df2 = pd.read_csv(csv_file_path, names=col_names, usecols=cols2, skiprows=skip, nrows=1)
+    print('Sample data for SessionID to ClientIP edge')
+    print('------------------------------------------')
+    print(df2)
+    print('')
+    df3 = pd.read_csv(csv_file_path, names=col_names, usecols=cols3, skiprows=skip, nrows=1)
+    print('Sample data for SessionID to DomainName edge')
+    print('--------------------------------------------')
+    print(df3)
+    print('')
+    df4 = pd.read_csv(csv_file_path, names=col_names, usecols=cols4, skiprows=skip, nrows=1)
+    print('Sample data for SessionID to DeviceID edge')
+    print('------------------------------------------')
+    print(df4)
+    print('')
+    df5 = pd.read_csv(csv_file_path, names=col_names, usecols=cols5, skiprows=skip, nrows=1)
+    print('Sample data for SessionID to User edge')
+    print('--------------------------------------')
+    print(df5)
+    print('')
+except Exception as e:
+    print(f"Unexpected exception : {str(e)}")
+    raise e
