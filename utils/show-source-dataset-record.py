a4276b3692f00a383ea71c57e8f8d14420786f3b
snypher <chvezk@amazon.com>
2022-06-27 14:35:37 +0000

    Update conditional code to define column names in util script show-source-dataset-record.py

--- utils/show-source-dataset-record.py
+++ utils/show-source-dataset-record.py
@@ -17,25 +17,23 @@ try:
     if args.csv_file is None or args.dataset is None:
         raise Exception("A CSV file must be provided as input")
     
-    if args.dataset is 'first-party':
+    if args.dataset == 'first-party':
         col_names = [ 'user_name', 'email', 'phone_number', 'external_id', 
             'street_address', 'postcode', 'city', 'country', 'birthday', 
             'loyalty_points', 'loyalty_level' ]
-        cols = [ 0, 1, 2, 3 , 4, 5, 6, 7, 8, 9, 10 ]
-    elif args.dataset is 'cookie':
+    elif args.dataset == 'cookie':
         col_names = [ 'cookie_id', 'session_id', 'last_action', 'user_name', 
         'conversion_id', 'session_duration_sec' ]
-        cols = [ 0, 1, 2, 3 , 4, 5 ]
-    elif args.dataset is 'clickstream':
+    elif args.dataset == 'clickstream':
         col_names = [ 'session_id', 'client_ip', 'client_platform', 
         'canonical_url', 'domain_name', 'app_id', 'device_id', 'user_name', 
         'events', 'start_timestamp', 'start_event', 'end_timestamp', 
         'end_event', 'session_duration_sec', 'user-agent' ]
-        cols = [ 0, 1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ]
-    else:    # Anything else is 'transactional'
+    elif args.dataset == 'transactional':    # Anything else is 'transactional'
         col_names = [ 'purchase_id', 'product_name', 'purchased_date', 
         'product_category', 'customer_id', 'reward_points' ]
-        cols = [ 0, 1, 2, 3 , 4, 5 ]
+    else:
+        raise Exception("A valid dataset must be selected")
     
     csv_file_path = args.csv_file
     lines = len(pd.read_csv(csv_file_path))
@@ -44,8 +42,7 @@ try:
     print('Randomly selecting the {} record # {}'.format(args.dataset,skip))
     print('')
     
-    df = pd.read_csv(csv_file_path, names=col_names, 
-        usecols=cols, skiprows=skip, nrows=1)
+    df = pd.read_csv(csv_file_path, names=col_names, skiprows=skip, nrows=1)
     print('Mock data for a single {} record'.format(args.dataset))
     print('----------------------------------------')
     print(df.squeeze())
